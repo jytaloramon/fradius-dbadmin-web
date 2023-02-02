1e506cdfcb3b7a5f1c5f1a08a09508b1852d8f51
Ramon Almeida <ramongoncalves76@gmail.com>
2023-02-02 12:44:52 -0300

    feat: add font awesome

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue';
 import appPinia from './modules/appPinia';
 import appRouter from './modules/appRouter';
 import appI18n from './modules/appI18n';
+import appIconsLibrary from './modules/appIconsLibrary';
 
 import './assets/main.css';
 
@@ -12,5 +13,7 @@ const app = createApp(App);
 app.use(appPinia);
 app.use(appRouter);
 app.use(appI18n);
+
+app.component('font-awesome-icon', appIconsLibrary);
 
 app.mount('#app');
---
+++ src/modules/appIconsLibrary.ts
@@ -0,0 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faUserSecret);
+
+export default FontAwesomeIcon;
