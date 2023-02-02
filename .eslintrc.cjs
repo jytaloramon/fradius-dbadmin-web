f432ddb6a55b9fe0a767656bec59f942417b2797
Ramon Almeida <ramongoncalves76@gmail.com>
2023-02-02 11:48:25 -0300

    style: changed - indent-tab: 2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,19 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-    root: true,
-    extends: [
-        'plugin:vue/vue3-essential',
-        'eslint:recommended',
-        '@vue/eslint-config-typescript',
-        '@vue/eslint-config-prettier',
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-    },
-    rules: {
-        semi: ['error', 'always'],
-        quotes: ['warn', 'single'],
-        indent: ['warn', 4],
-    },
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['warn', 'single'],
+    indent: ['warn', 2],
+  },
 };
--- src/modules/appRouter.ts
+++ src/modules/appRouter.ts
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import routes from '../router';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
 
 export default router;
