27ccf72c7d0fada0cefbb1b0dbb271284424e98c
Ramon Almeida <ramongoncalves76@gmail.com>
2023-02-02 11:32:25 -0300

    feat: moved modules

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import appPinia from './modules/appPinia';
+import appRouter from './modules/appRouter';
 
-import './assets/main.css'
+import './assets/main.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(appPinia);
+app.use(appRouter);
 
-app.mount('#app')
+app.mount('#app');
