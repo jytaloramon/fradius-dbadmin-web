3f9f9c179c64813843bc740b7e4e661a6b6a7085
Ramon Almeida <ramongoncalves76@gmail.com>
2023-02-08 00:32:24 -0300

    refactor: menu and header

---
+++ src/components/sidebar-menu/SideBarMenuHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mt-1 ml-2 mb-5 py-1 px-2 flex justify-between">
+    <div>
+      <span class="text-3xl underline text-green-600 font-bold italic">FR</span>
+      <span class="ml-1 text-white text-xl font-semibold">DbAdmin</span>
+    </div>
+
+    <div>
+      <a
+        class="w-8 h-8 absolute mt-1 -ml-2 p-1 rounded-full bg-green-600 hover:bg-green-700 text-center"
+      >
+        <font-awesome-icon
+          icon="fa-angle-double-left"
+          size="xs"
+          color="white"
+        />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SideBarMenuHeader',
+});
+</script>
--- src/components/sidebar-menu/TheSideBarMenu.vue
+++ src/components/sidebar-menu/TheSideBarMenu.vue
@@ -1,247 +1,26 @@
 <template>
   <aside
-    class="w-[240px] py-4 px-1 h-screen bg-black flex flex-col justify-between divide-y divide-dashed"
+    class="w-[240px] h-screen py-4 px-1 flex flex-col justify-between bg-neutral-900 text-neutral-300"
   >
-    <div class="divide-y divide-dotted">
-      <div class="flex mx-5 py-4 flex-row">
-        <span>
-          <img
-            class="w-[48px] h-[48px] rounded-full border-4 bo"
-            src="https://4maos.com.br/wp-content/uploads/2022/10/e4031b6fdcd96b53c78b2e3a12f6819c.jpg"
-            alt="Profile Picture"
-          />
-        </span>
+    <div>
+      <side-bar-menu-header />
 
-        <span class="ml-3 mt-1 text-slate-100">
-          <h3 class="text-xs">{{ $t('message.menuGreeting') }},</h3>
-          <h2 class="text-sm font-bold">Ytalo Ramon</h2>
-        </span>
-
-        <span
-          class="absolute ml-[200px] w-8 h-8 p-1 rounded-full bg-green-600 text-center"
-        >
-          <a>
-            <font-awesome-icon
-              icon="fa-angle-double-left"
-              size="xs"
-              color="white"
-            />
-          </a>
-        </span>
-      </div>
-
-      <div class="py-5 text-blue-50">
-        <h2 class="mx-5 text-xs">{{ $t('label.menu') }}: {{ getSizeMenu }}</h2>
-
-        <ul class="mx-3 mt-4">
-          <side-bar-menu-item-atomic
-            @item-click="updateItemSelected(menuItem.dashboard)"
-            :item-prop="menuItem.dashboard"
-          />
-
-          <side-bar-menu-item-atomic
-            @item-click="updateItemSelected(menuItem.user)"
-            :item-prop="menuItem.user"
-          />
-
-          <side-bar-menu-item-atomic
-            @item-click="updateItemSelected(menuItem.nas)"
-            :item-prop="menuItem.nas"
-          />
-        </ul>
-      </div>
+      <side-bar-menu-main />
     </div>
 
-    <div class="pt-2 text-blue-50">
-      <ul class="mx-3 mt-auto">
-        <side-bar-menu-item-atomic
-          @item-click="signOut"
-          :item-prop="menuItem.signOut"
-        />
-
-        <side-bar-menu-item-atomic
-          @item-click="changeTheme"
-          :item-prop="menuItem.themeMode"
-        >
-          <span
-            class="my-auto ml-auto w-8 h-4 py-0.5 px-1 bg-white rounded-2xl"
-          >
-            <div class="w-3 h-3 rounded-full bg-black float-right"></div>
-          </span>
-        </side-bar-menu-item-atomic>
-      </ul>
-    </div>
+    <side-bar-menu-footer />
   </aside>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import SideBarMenuItemAtomic from './SideBarMenuItemAtomic.vue';
-import type { ISideBarMenuItemAtomic } from './interfaces/ISideBarMenuItemAtomic';
-import StyleFactory from '@/common/components/factory/StyleFactory';
+import SideBarMenuHeader from './SideBarMenuHeader.vue';
+import SideBarMenuMain from './SideBarMenuMain.vue';
+import SideBarMenuFooter from './SideBarMenuFooter.vue';
 
 export default defineComponent({
   name: 'TheSideBarMenu',
-  components: { SideBarMenuItemAtomic },
-  data() {
-    return {
-      menuItem: {
-        dashboard: {
-          style: StyleFactory.create(['my-1']),
-          box: {
-            style: StyleFactory.create([
-              'flex',
-              'flex-row',
-              'p-2',
-              'rounded-lg',
-              'hover:bg-gray-700',
-            ]),
-            spanPicture: {
-              style: StyleFactory.create(['ml-1']),
-              picture: {
-                icon: 'fa-solid fa-chart-simple',
-                color: 'white',
-              },
-            },
-            spanText: {
-              style: StyleFactory.create(['ml-3', 'my-auto', 'text-sm']),
-              text: this.$t('label.dashboard'),
-            },
-          },
-        },
-        user: {
-          style: StyleFactory.create(['my-1']),
-          box: {
-            style: StyleFactory.create([
-              'flex',
-              'flex-row',
-              'p-2',
-              'rounded-lg',
-              'hover:bg-gray-700',
-            ]),
-            spanPicture: {
-              style: StyleFactory.create(['ml-1']),
-              picture: {
-                icon: 'fa-solid fa-users',
-                color: 'white',
-              },
-            },
-            spanText: {
-              style: StyleFactory.create(['ml-3', 'my-auto', 'text-sm']),
-              text: this.$t('label.user'),
-            },
-          },
-        },
-        nas: {
-          style: StyleFactory.create(['my-1']),
-          box: {
-            style: StyleFactory.create([
-              'flex',
-              'flex-row',
-              'p-2',
-              'rounded-lg',
-              'hover:bg-gray-700',
-            ]),
-            spanPicture: {
-              style: StyleFactory.create(['ml-1']),
-              picture: {
-                icon: 'fa-solid fa-ethernet',
-                color: 'white',
-              },
-            },
-            spanText: {
-              style: StyleFactory.create(['ml-3', 'my-auto', 'text-sm']),
-              text: this.$t('label.nas'),
-            },
-          },
-        },
-        signOut: {
-          style: StyleFactory.create(['my-1']),
-          box: {
-            style: StyleFactory.create([
-              'flex',
-              'flex-row',
-              'p-2',
-              'rounded-lg',
-              'hover:bg-gray-700',
-            ]),
-            spanPicture: {
-              style: StyleFactory.create(['ml-1']),
-              picture: {
-                icon: 'fa-solid fa-right-to-bracket',
-                color: 'white',
-                rotation: '180',
-              },
-            },
-            spanText: {
-              style: StyleFactory.create(['ml-3', 'my-auto', 'text-sm']),
-              text: this.$t('label.signOut'),
-            },
-          },
-        },
-        themeMode: {
-          style: StyleFactory.create(['my-1']),
-          box: {
-            style: StyleFactory.create([
-              'flex',
-              'flex-row',
-              'p-2',
-              'rounded-lg',
-              'hover:bg-gray-700',
-            ]),
-            spanPicture: {
-              style: StyleFactory.create(['ml-1']),
-              picture: {
-                icon: 'fa-solid fa-sun',
-                color: 'white',
-              },
-            },
-            spanText: {
-              style: StyleFactory.create(['ml-3', 'my-auto', 'text-sm']),
-              text: this.$t('label.lightMode'),
-            },
-          },
-        },
-      },
-      menuItemSelected: {} as ISideBarMenuItemAtomic,
-    };
-  },
-  beforeMount() {
-    this.updateItemSelected(this.menuItem.dashboard);
-  },
-  computed: {
-    getSizeMenu(): number {
-      return 3;
-    },
-  },
-  methods: {
-    updateItemSelected(newItem: ISideBarMenuItemAtomic): void {
-      if (this.menuItemSelected.box) {
-        if (this.menuItemSelected == newItem) return;
-
-        StyleFactory.setPropety(
-          this.menuItemSelected.box.style,
-          'hover:bg-gray-700'
-        );
-        StyleFactory.setPropety(
-          this.menuItemSelected.box.style,
-          'bg-green-600',
-          false
-        );
-      }
-
-      StyleFactory.setPropety(newItem.box.style, 'hover:bg-gray-700', false);
-      StyleFactory.setPropety(newItem.box.style, 'bg-green-600');
-
-      this.menuItemSelected = newItem;
-    },
-    signOut(): void {
-      return;
-    },
-    changeTheme(): void {
-      return;
-    },
-  },
+  components: { SideBarMenuHeader, SideBarMenuMain, SideBarMenuFooter },
 });
 </script>
