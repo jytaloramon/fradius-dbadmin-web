dcd020f6a6d623819a29528964a6d4fc08f2e9a2
Ramon Almeida <ramongoncalves76@gmail.com>
2023-02-22 13:46:59 -0300

    feat: add topbat

---
+++ src/components/topbar/TopBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full h-20 p-2 text-black">
+    <h2 class="ml-5 mt-4 text-2xl text-neutral-600 font-semibold">Usuário</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TopBar',
+});
+</script>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <the-side-bar-menu />
+  <div class="w-full flex">
+    <the-side-bar-menu class="w-2/12" />
+
+    <div class="w-10/12">
+      <top-bar />
+
+      <user-table />
+    </div>
   </div>
 </template>
 
@@ -8,11 +14,15 @@
 import { defineComponent } from 'vue';
 
 import TheSideBarMenu from '../components/sidebar-menu/TheSideBarMenu.vue';
+import TopBar from '../components/topbar/TopBar.vue';
+import UserTable from '../components/user/UserTable.vue';
 
 export default defineComponent({
   name: 'MainPage',
   components: {
     TheSideBarMenu,
+    TopBar,
+    UserTable,
   },
 });
 </script>
