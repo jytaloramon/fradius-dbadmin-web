e9e86cb2d7a3f57954e2117f052616c6e37b87c9
Ramon Almeida <ramongoncalves76@gmail.com>
2023-02-21 23:37:04 -0300

    feat: VTable and VTableRow

---
+++ src/common/components/table/main/VTableRow.vue
@@ -0,0 +1,21 @@
+<template>
+  <th :class="rowProp.style">
+    <slot></slot>
+  </th>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue';
+
+import type { IVTableRow } from './interfaces/IVTableRow';
+
+export default defineComponent({
+  name: 'VTableRow',
+  props: {
+    rowProp: {
+      type: Object as PropType<IVTableRow>,
+      required: true,
+    },
+  },
+});
+</script>
---
+++ src/common/components/table/main/interfaces/IVTable.ts
@@ -0,0 +1,5 @@
+import type { IStyle } from '@/common/components/interfaces/IStyle';
+
+export interface IVTable {
+  style?: IStyle;
+}
